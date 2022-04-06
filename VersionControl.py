cfdfee937c88a682afdaebebce13a6335c76b29f
twelfthdoctor1 <officialtwelfthdoctor1@hotmail.com>
2022-04-07 02:15:42 +0800

    Version 0.3.2 Commit.

--- VersionControl.py
+++ VersionControl.py
@@ -5,5 +5,5 @@ __credits__ = "TwelfthDoctor1"
 __license__ = "CC 4.0 or MIT"
 
 # Version Control Datum
-__version__ = "Developer Version 0.3.1"
+__version__ = "Developer Version 0.3.2"
 __status__ = "Development Testing"
